53bd4c1d20f805406e34376f88c53f4e1a82ffd2
Eugene <zh_kononenko3@mail.ru>
2023-03-16 00:26:31 +0300

    Fix documentPicker

--- screens/BookScreen/BookScreen.tsx
+++ screens/BookScreen/BookScreen.tsx
@@ -4,49 +4,12 @@ import React, { useEffect, useState } from 'react';
 import { bookFile } from '../../assets/bookFile';
 import { styles } from './styles';
 import * as DocumentPicker from 'expo-document-picker';
+import { StorageAccessFramework } from 'expo-file-system';
 
 export function BookScreen({ navigation }: any) {
 
     let filePath = FileSystem.documentDirectory + 'example.txt';
-    // const [text, setText] = useState('Пусто');
     let content = bookFile.text;
-
-    async function makeFile() {
-        try {
-            //create a file at filePath. Write the content data to it
-            await FileSystem.writeAsStringAsync(filePath, content);
-            console.log("written to file");
-        }
-        catch (error) { //if the function throws an error, log it out.
-            console.log(error);
-        }
-    };
-
-    async function readText(path: string) {
-        await FileSystem.readAsStringAsync(path)
-            .then(text => navigation.navigate('Reader', { content: text }));
-    }
-
-    async function AddBookFromFile() {
-        let result = await DocumentPicker.getDocumentAsync({ type: 'text/*' });
-
-        if (result.type == 'success') {
-            readText(FileSystem.cacheDirectory + result.name);
-            console.log(result.uri);
-        }
-
-
-        // const cachePath = "file:///data/user/0/host.exp.exponent/cache/DocumentPicker/5a593e44-1c98-4795-98cd-391687fc9f6f.txt";
-        // FileSystem.readAsStringAsync(cachePath)
-        //     .then(text => navigation.navigate('Reader', { content: text }));
-    }
-
-    const load = async () => {
-        const res = await DocumentPicker.getDocumentAsync({ type: 'text/*' })
-        if (res.type === "success") {
-            readText(res.uri)
-        } 
-    }
 
     const requestReadExternalStoragePermission = async () => {
         try {
@@ -63,6 +26,38 @@ export function BookScreen({ navigation }: any) {
         }
     };
 
+    async function makeFile() {
+        try {
+            //create a file at filePath. Write the content data to it
+            await FileSystem.writeAsStringAsync(filePath, content);
+            console.log("written to file");
+        }
+        catch (error) { //if the function throws an error, log it out.
+            console.log(error);
+        }
+    };
+
+    async function readText(path: string) {
+        // await FileSystem.readAsStringAsync(path)
+        //     .then(text => navigation.navigate('Reader', { content: text }));
+        await FileSystem.StorageAccessFramework.readAsStringAsync(path)
+            .then(text => navigation.navigate('Reader', { content: text }));
+    }
+    
+
+    async function AddFromFile() {
+        const res = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: false, type: 'text/*' });
+        if (res.type === "success") {
+            // await FileSystem.copyAsync({ from: res.uri, to: FileSystem.documentDirectory });
+            // console.log(FileSystem.documentDirectory + res.name);
+            
+            readText(res.uri);
+            
+        }
+    }
+
+    
+
 
     return (
         <View >
@@ -71,7 +66,7 @@ export function BookScreen({ navigation }: any) {
                 <Button title='Make file' onPress={makeFile} />
                 <Button title='Give storage permission' onPress={requestReadExternalStoragePermission} />
                 <Button title='read text' onPress={() => readText(filePath)} />
-                <Button title='Add book from file' onPress={load} />
+                <Button title='Add book from file' onPress={AddFromFile} />
 
             </View>
         </View>
